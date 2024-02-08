a96d35452906c5a7098dd012bc9d12dc0b53568d
genfilweb <142978168+genfilweb@users.noreply.github.com>
2024-02-08 13:22:04 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,33 @@
+    }
+        console.log("Random Number: " + item);
+main();
+
+    data.forEach(item => {
+}
+    const data = [];
+    });
+    return data;
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = generateRandomData();
+}
+}
+    const data = generateRandomData();
+    data.forEach(item => {
+main();
+    }
+function main() {
+
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+
+    const data = [];
+    });
+    for (let i = 0; i < 10; i++) {
+}
+    return data;
+        console.log("Random Number: " + item);
